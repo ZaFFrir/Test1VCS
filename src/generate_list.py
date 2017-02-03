a5c628893b3d7580c4b0ccef7a75860866975dc2
ZaFFrir <ph.punyapa_st@tni.ac.th>
2017-02-03 02:40:11 +0000

    C4-generate_list v2

--- src/generate_list.py
+++ src/generate_list.py
@@ -1,6 +1,8 @@
 import random
 def generate_list():
      alist = [x for x in range (random.randint(-10, 10))]
+     assert len(alist) != 0 ,"the list must empty"
+     assert sum(alist) > -100, "sum of list must < -100"
      return alist
      
 """
